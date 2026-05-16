a12d370302bfb393b42759e4d130193b4f3e1f58
Maciej Paszek <maciejpaszek@hotmail.com>
2026-05-16 22:21:31 +0200

    16.05.2026 - OStatni raz, kiedy żyroskop liczył na int
    Teraz będzie liczył float

--- TaskReadGyro.ino
+++ TaskReadGyro.ino
@@ -9,82 +9,74 @@
 
 void TaskReadGyro(void *)
 {
+  // Kod błędu
   byte error;
-  char buffer[200];
 
+  // Łańcuch znakó dla komunikatów o błędach
+  char buffer[255];
+
+  // Obiekt klasy MPU6050
   MPU6050 mpu6050 = MPU6050();
 
+  // Otwieranie połączenia
   error = mpu6050.begin(SDA, SCL, MPU6050_ADR);
   
-  //if(error != MPU6050_NO_ERROR)
+  if(error != MPU6050_NO_ERROR)
   {
     sprintf(buffer, "begin error: 0x%02x\n", error);
     Serial.print(buffer);
   }
 
-  //mpu6050.setRange(MPU6050_RANGE_2000_DEG_PER_SEC);
-  error = mpu6050.setRange(MPU6050_RANGE_2000_DEG_PER_SEC);
+  // Ustalanie zakresu żyroskopu
+  // - MPU6050_RANGE_250_DEG_PER_SEC
+  // - MPU6050_RANGE_500_DEG_PER_SEC
+  // - MPU6050_RANGE_1000_DEG_PER_SEC
+  // - MPU6050_RANGE_2000_DEG_PER_SEC
+  error = mpu6050.setRange(MPU6050_RANGE_250_DEG_PER_SEC);
 
-  //if(error != MPU6050_NO_ERROR)
+  if(error != MPU6050_NO_ERROR)
   {
     sprintf(buffer, "setRange error: 0x%02x\n", error);
     Serial.print(buffer);
   }
 
+  // Zmienne na orientację żyroskopu (całka z prędkości)
   int X = 0;
   int Y = 0;
   int Z = 0;
   
+  int eps = 1000;
+
   while(true)
   {
     // Wykonaj pomiar z żyroskopu
     mpu6050.measure();
 
-    //gyroX = mpu6050.gX;
-    //gyroY = mpu6050.gY;
-    //gyroZ = mpu6050.gZ;
 
-    if(mpu6050.gX == 0 & mpu6050.gY == 0 & mpu6050.gZ == 0)
+
+    // Całka z prędkości strefa nieczułości
+    if(mpu6050.gX < -eps || mpu6050.gX > eps)
     {
-      //mpu6050.reset();
+      X += mpu6050.gX;
     }
 
-    X += mpu6050.gX;
-    Y += mpu6050.gY;
-    Z += mpu6050.gZ;
+    if(mpu6050.gY < -eps || mpu6050.gY > eps)
+    {
+      Y += mpu6050.gY;
+    }
 
+    if(mpu6050.gZ < -eps || mpu6050.gZ > eps)
+    {
+      Z += mpu6050.gZ;
+    }
+
+ 
+
+    // Wypisz
     sprintf(buffer, "gX:%d;\tgY:%d;\tgZ:%d;\tX:%d;\tY:%d;\tZ:%d", mpu6050.gX, mpu6050.gY, mpu6050.gZ, X, Y, Z);
     Serial.println(buffer);
 
+    // Opóźnienie 100 ms
     vTaskDelay(100 / portTICK_PERIOD_MS);
   }
-}
-
-bool ReadGyroscope(int &gyroX, int &gyroY, int &gyroZ)
-{
-  Wire.beginTransmission(MPU6050_ADR); 
-  Wire.write(GYRO_XOUT_H); // used as starting address to read all other values
-  uint8_t error = Wire.endTransmission(false); // stores success/error code in variable and keeps sensor active as we need to read data
-
-  Wire.requestFrom(MPU6050_ADR, 6, true);
-
-  gyroX = Wire.read() << 8 | Wire.read(); 
-  gyroY = Wire.read() << 8 | Wire.read();
-  gyroZ = Wire.read() << 8 | Wire.read();
-
-  gyroX = Int16ToInt32(gyroX);
-  gyroY = Int16ToInt32(gyroY);
-  gyroZ = Int16ToInt32(gyroZ);
-
-  return true;
-}
-
-int Int16ToInt32(int int16)
-{
-  if(int16 >= 0b1000000000000000)
-  {
-    return int16 - 0b10000000000000000;
-  }
-
-  return int16;
 }
